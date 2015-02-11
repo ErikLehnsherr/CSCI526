ab0e9a8a62df757986568cac456eaf56f0ea9c75
ErikLehnsherr <jotaro.chen@gmail.com>
2015-02-11 00:04:29 -0800

    Integration

---
+++ Assets/Scripts/FireBall.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FireBall : MonoBehaviour {
+
+
+    //Private Elements
+    //private Vector3 forwardVect;
+    private float timeForFire = 0;
+   // private float speedFire = 70;
+    private float fireBallExistPeriod = 3; 
+
+    //Public Elements
+    public List <Transform> Enemies;
+    public Transform SelectedTarget;
+    
+    
+
+	// Use this for initialization
+	void Start () {
+
+        //forwardVect = Vector3.forward;
+		
+		SelectedTarget = null;
+        Enemies = new List<Transform>();
+        AddEnemiesToList();
+ 
+
+	}
+	
+	public void AddEnemiesToList()
+     {
+         GameObject[] ItemsInList = GameObject.FindGameObjectsWithTag("Enemy");
+         foreach(GameObject _Enemy in ItemsInList)
+         {
+             AddTarget(_Enemy.transform);
+         }
+     }
+	 
+	 
+	 public void AddTarget(Transform enemy)
+     {
+         Enemies.Add(enemy);
+     }
+	 
+	 public void DistanceToTarget()
+     {
+         Enemies.Sort(delegate( Transform t1, Transform t2){ 
+             return Vector3.Distance(t1.transform.position,transform.position).CompareTo(Vector3.Distance(t2.transform.position,transform.position)); 
+         });
+     
+     }
+	 
+	 public void TargetedEnemy() 
+     {
+         if(SelectedTarget == null)
+         {
+             DistanceToTarget();
+             SelectedTarget = Enemies[0];
+         }
+     }
+	 
+	 
+	// Update is called once per frame
+	void Update () {
+
+		TargetedEnemy();
+         float dist = Vector3.Distance(SelectedTarget.transform.position,transform.position);
+         //if(dist <150)
+         //{
+             transform.position = Vector3.MoveTowards(transform.position, SelectedTarget.position, 60 * Time.deltaTime);
+         //}
+	
+	
+         timeForFire += Time.deltaTime;
+       // this.gameObject.transform.Translate(forwardVect * speedFire * Time.deltaTime, Space.Self);
+
+
+        if (timeForFire > fireBallExistPeriod)
+        {
+
+            timeForFire = 0;
+            GameObject.Destroy(this.gameObject);
+            
+        } 
+
+	}
+}
---
+++ Assets/Scripts/ZionScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZionScript : MonoBehaviour {
+
+
+    public GameObject FireBall;
+    private Vector3 firePoint;
+
+    private GameObject FB;
+    private float bulletSpawner = 0;
+    private float newTimerExistPeriod = 2;
+
+	// Use this for initialization
+	void Start () {
+
+        firePoint = new Vector3(this.transform.position.x, this.transform.position.y + 5, this.transform.position.z);
+
+        FB = (GameObject)Instantiate(FireBall, firePoint, Quaternion.identity);
+    
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        bulletSpawner += Time.deltaTime;
+
+        if (bulletSpawner > newTimerExistPeriod)
+        {
+            FB = (GameObject)Instantiate(FireBall, firePoint, Quaternion.identity);
+            bulletSpawner = 0;
+        }
+
+	}
+}
